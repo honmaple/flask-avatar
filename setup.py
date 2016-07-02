130483326d2e888a07020a36c7d5ae36e496ed56
honmaple <xiyang0807@gmail.com>
2016-07-02 18:43:14 +0800

    add readme and upload to pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 # Author: jianglin
 # Email: xiyang0807@gmail.com
 # Created: 2016-07-02 16:12:02 (CST)
-# Last Update:星期六 2016-7-2 16:22:2 (CST)
+# Last Update:星期六 2016-7-2 18:31:25 (CST)
 #          By:
 # Description:
 # **************************************************************************
@@ -20,7 +20,7 @@ setup(
     license='BSD',
     author='honmaple',
     author_email='xiyang0807@gmail.com',
-    description='to generate avatar with flask',
+    description='To generate avatar for flask',
     long_description='Please visit https://github.com/honmaple/flask-avatar',
     packages=['flask_avatar'],
     zip_safe=False,
